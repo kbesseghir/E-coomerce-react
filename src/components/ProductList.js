eca859909cd3b46abe58aebacd703aa7e89716e7
kbesseghir <k.besseghir@esi-sba.dz>
2023-11-21 11:12:09 +0100

    commit

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,23 @@ function ProductList(){
     return(
         <>
 
-        <h2 className="text-center">  Our Product</h2>
+        <h2 className="text-center p-5">  Our Products</h2>
+        <div className="container">
+            <div className="row">
+                <div className="col-3">
+
+                <div class="card" >
+                <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <button class="btn btn-primary">Go somewhere</button>
+                </div>
+                </div>
+                </div>
+                
+            </div>
+        </div>
         </>
     );
 }
