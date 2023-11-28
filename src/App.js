a2ef4faa9fd5b87a5413e741da2bb3bcccfb0b88
kbesseghir <k.besseghir@esi-sba.dz>
2023-11-28 14:22:28 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Slider from "./components/Slider";
 import './index.css'
+import { Route,routes } from "react-router-dom";
 
 function App() {
   return (
