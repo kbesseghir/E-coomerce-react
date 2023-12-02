f62d7ef306b3dbdc67eca6046ff27a443cdbbe91
kbesseghir <k.besseghir@esi-sba.dz>
2023-12-02 22:11:23 +0100

    commit

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,23 +1,25 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Product from "./Product";
 
-function Details (){
-    const apiUrl='https://fakestoreapi.com/products'
-    const params = useParams()
-    const [product,setProduct]= useState({})
-    console.log(params)
-    useEffect(()=>{
-    fetch(`${apiUrl}/${params.productId}`).then((response) => response.json())
-    .then((data) => setProduct(data));
-    },[])
-    return (
-        <>
-            <h2>Product Details ({product.title}){params.productId}</h2>
-            <Product  product ={product}  showButton={false}/>
-        </>
-
-    );
+function Details() {
+  const apiUrl = "https://fakestoreapi.com/products";
+  const params = useParams();
+  const [product, setProduct] = useState({});
+  console.log(params);
+  useEffect(() => {
+    fetch(`${apiUrl}/${params.productId}`)
+      .then((response) => response.json())
+      .then((data) => setProduct(data));
+  }, []);
+  return (
+    <>
+      <h2>
+        Product Details ({product.title}){params.productId}
+      </h2>
+      <Product product={product} showButton={false} />
+    </>
+  );
 }
 
 export default Details;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,45 +5,66 @@ function ProductList() {
   const apiUrl = "https://fakestoreapi.com/products";
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
-  const getproInCat=(cat)=>{
-    return (fetch(`${apiUrl}/category/${cat}`)
-    .then((response) => response.json())
-    .then((data) => setProducts(data)));}
-  
-  
-  
+  const getproInCat = (cat) => {
+    return fetch(`${apiUrl}/category/${cat}`)
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  };
 
+  const getProducts = () => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  };
 
-  const getProducts = ()=>{fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => setProducts(data));}
-
-  const getCategories = ()=>{fetch(`${apiUrl}/categories`)
-    .then((response) => response.json())
-    .then((data) => setCategories(data));}
+  const getCategories = () => {
+    fetch(`${apiUrl}/categories`)
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  };
   useEffect(() => {
-    getProducts()
-    getCategories()
-  },[]);
+    getProducts();
+    getCategories();
+  }, []);
 
   return (
     <>
       <h2 className="text-center p-5"> Our Products</h2>
-      {categories.map((cat)=> {
-        return  <button className="btn btn-info"  onClick={getproInCat()} key={cat}>{cat}</button>
-    })}
+      
+     
 
       <div className="container">
-        <div className="row">
-          {products.map((product)=>{
+      <button
+            className="btn btn-info"
+            onClick={() => {
+              getProducts() ;
+            }}
+            
+          >
+            All
+          </button>
+          {categories.map((cat) => {
             return (
               
-            <div className="col-3" key={product.id}>
-              <Product product={product} showButton={true} />
-            </div>
-              
-          )})}
-          
+              <button
+                className="btn btn-info"
+                onClick={() => {
+                  getproInCat(cat);
+                }}
+                key={cat}
+              >
+                {cat}
+              </button>
+            );
+          })}
+        <div className="row">
+          {products.map((product) => {
+            return (
+              <div className="col-3" key={product.id}>
+                <Product product={product} showButton={true} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
